95e142138aa1a0f3c1fc98e33f2f9af9ccfbaff2
seoseokkyo <seoseokkyo@naver.com>
2024-05-01 17:52:13 +0900

    레시피 관련 작업
    레시피 기본 틀 작업
    레시피 예시 작업
    주문기능(조리가 필요한 레시피를 정하는 기능) 추가

--- CookingSimulator/Source/CookingSimulator/Private/CookingSimulatorGameModeBase.cpp
+++ CookingSimulator/Source/CookingSimulator/Private/CookingSimulatorGameModeBase.cpp
@@ -2,4 +2,116 @@
 
 
 #include "CookingSimulatorGameModeBase.h"
+#include "CookingSimulatorGameInstance.h"
 
+void ACookingSimulatorGameModeBase::StartPlay()
+{
+	auto gameInstance = CastChecked<UCookingSimulatorGameInstance>(GetGameInstance());
+
+	if (gameInstance != nullptr)
+	{
+		recipes.Reset();
+
+		// 햄버거 레시피 작성
+		{
+			// 레시피 작성
+			FCookingSimulatorRecipeInfo recipeDetail;
+
+			recipeDetail.recipeName = TEXT("Hamburger");
+
+			// 레시피 타입 설정
+			recipeDetail.recipeType = ECookingSimulatorRecipeType::Hamburger;
+
+			// 레시피 이미지 경로 설정 예시
+			recipeDetail.recipeImagePath = "Content\\CookingSimulator\\Textures\\Hamburger.png";
+
+			// 레시피 설명 설정 예시
+			recipeDetail.recipeDescription = "This is Hamburger";
+
+			// 레시피에 들어가는 식재료 설정
+			recipeDetail.ingredientInfoArray.Reset();
+
+			// 토마토 추가 예시
+			FString ingredientName = TEXT("Tomato");
+			recipeDetail.ingredientInfoArray.Add(FIngredientInfo(ingredientName, gameInstance->GetItemDataTable(ingredientName), 50, 100));
+
+			recipeDetail.cookingTimeLimit = 600;
+
+			// 햄버거 레시피 작성 완료
+			recipes.Add(recipeDetail);
+		}
+
+		// 연어스테이크와 삶은감자 레시피 작성
+		{
+			// 레시피 작성
+			FCookingSimulatorRecipeInfo recipeDetail;
+
+			recipeDetail.recipeName = TEXT("SalmonSteakAndBoiledPotato");
+
+			// 레시피 타입 설정
+			recipeDetail.recipeType = ECookingSimulatorRecipeType::SalmonSteakAndBoiledPotato;
+
+			// 레시피 이미지 경로 설정 예시
+			recipeDetail.recipeImagePath = "Content\\CookingSimulator\\Textures\\SalmonSteakAndBoiledPotato.png";
+
+			// 레시피 설명 설정 예시
+			recipeDetail.recipeDescription = "This is SalmonSteakAndBoiledPotato";
+
+			// 레시피에 들어가는 식재료 설정
+			recipeDetail.ingredientInfoArray.Reset();
+
+			// 감자 추가 예시
+			FString ingredientName = TEXT("Potato");
+			recipeDetail.ingredientInfoArray.Add(FIngredientInfo(ingredientName, gameInstance->GetItemDataTable(ingredientName), 50, 100));
+
+			recipeDetail.cookingTimeLimit = 600;
+
+			// 연어스테이크와 삶은감자 레시피 작성 완료
+			recipes.Add(recipeDetail);
+		}
+	}
+}
+
+void ACookingSimulatorGameModeBase::Tick(float DeltaSeconds)
+{
+	if (bCooking)
+	{
+		cookingTimer += DeltaSeconds;
+
+		if (cookingTimer >= currentRecipe.cookingTimeLimit)
+		{
+			cookingTimer = 0;
+			bCooking = false;
+
+			UE_LOG(LogTemp, Warning, TEXT("Cook Time Over"));
+		}
+	}
+}
+
+bool ACookingSimulatorGameModeBase::SetCurrentRecipe(ECookingSimulatorRecipeType eType)
+{
+	//// Random
+	//currentRecipe = recipes[FMath::RandRange(0, recipes.Num()-1)];
+
+	for (const FCookingSimulatorRecipeInfo& rcpInfo : recipes)
+	{
+		if (rcpInfo.recipeType == eType)
+		{
+			currentRecipe = rcpInfo;
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+FCookingSimulatorRecipeInfo ACookingSimulatorGameModeBase::GetRecipe(int32 recipeIndex)
+{
+	if (recipes.Num() - 1 >= recipeIndex)
+	{
+		return recipes[recipeIndex];	
+	}	
+
+	return FCookingSimulatorRecipeInfo();
+}
--- CookingSimulator/Source/CookingSimulator/Public/CookingSimulatorGameModeBase.h
+++ CookingSimulator/Source/CookingSimulator/Public/CookingSimulatorGameModeBase.h
@@ -4,14 +4,106 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Ingredient.h"
 #include "CookingSimulatorGameModeBase.generated.h"
 
 /**
  * 
  */
+
+ 
+UENUM(BlueprintType)
+enum class ECookingSimulatorRecipeType : uint8
+{
+	Hamburger,
+	SalmonSteakAndBoiledPotato
+};
+
+USTRUCT(BlueprintType)
+struct FIngredientInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	FString ingredientName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	FCookingSimulatorItemInfo ingredientInfo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	int32 ingredientWeight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	int32 ingredientCookedLevel;
+
+	FIngredientInfo(){}
+
+	FIngredientInfo(FString ingredientName, FCookingSimulatorItemInfo ingredientInfo, int32 ingredientWeight, int32 ingredientCookedLevel)
+	{
+		this->ingredientName = ingredientName;
+		this->ingredientInfo = ingredientInfo;
+		this->ingredientWeight = ingredientWeight;
+		this->ingredientCookedLevel = ingredientCookedLevel;
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FCookingSimulatorRecipeInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RecipeInfo")
+	ECookingSimulatorRecipeType recipeType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RecipeInfo")
+	FString recipeName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RecipeInfo")
+	FString recipeImagePath;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RecipeInfo")
+	FString recipeDescription;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RecipeInfo")
+	TArray<FIngredientInfo> ingredientInfoArray;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RecipeInfo")
+	float cookingTimeLimit;
+};
+
+
+
 UCLASS()
 class COOKINGSIMULATOR_API ACookingSimulatorGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+private:
+
+	FCookingSimulatorRecipeInfo currentRecipe;
+	bool bCooking;
+	float cookingTimer;
+
+protected:
+	virtual void StartPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Recipe")
+	TArray<FCookingSimulatorRecipeInfo> recipes;
+
+	UFUNCTION(BlueprintCallable)
+	bool SetCurrentRecipe(ECookingSimulatorRecipeType eType);
+
+	UFUNCTION(BlueprintCallable)
+	FCookingSimulatorRecipeInfo GetCurrentRecipe() { return currentRecipe;};
+
+	UFUNCTION(BlueprintCallable)
+	FCookingSimulatorRecipeInfo GetRecipe(int32 recipeIndex);
+
+	UFUNCTION(BlueprintCallable)
+	void CookingStart() { bCooking = true;};
 };
