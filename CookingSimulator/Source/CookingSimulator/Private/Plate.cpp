ec9e6f74b878f13800a8d7b2894024bdc4d18c95
seonmin-yoon <tjsals914@gmail.com>
2024-05-16 07:27:50 +0900

    레벨 작업

---
+++ CookingSimulator/Source/CookingSimulator/Private/Plate.cpp
@@ -0,0 +1,19 @@
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//
+//#include "Plate.h"
+//
+//APlate::APlate()
+//{
+//	plate = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("plate"));
+//	plate->SetupAttachment(RootComponent);
+//
+//}
+//
+//void APlate::BeginPlay()
+//{
+//}
+//
+//void APlate::Tick(float DeltaTime)
+//{
+//}
